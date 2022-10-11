09484db4c49d9dc15e7e5d5277b366455f4187f3
VeraKulemina <verakulemina@gmail.com>
2022-10-10 18:48:44 -0600

    commit tree 99305f532d00ad452cf4ba7c2147ca285fae7f0e

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import About from "/Users/verak/Development/code/phase-2/react-hooks-import-export-lab/src/components/About.js"
+import Home from "/Users/verak/Development/code/phase-2/react-hooks-import-export-lab/src/components/Home.js"
+import NavBar from "/Users/verak/Development/code/phase-2/react-hooks-import-export-lab/src/components/NavBar.js"
+
+
 
 function App() {
   return (
